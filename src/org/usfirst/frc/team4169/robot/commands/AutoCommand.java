e7e16f7b339574f46642d7a6ba35cce4016f519e
DESKTOP-8M6LA9U\jason <jason.huang.sudbury@gmail.com>
2018-02-13 17:01:00 -0500

    +Auto code
    Added some autonomous code

--- src/org/usfirst/frc/team4169/robot/commands/AutoCommand.java
+++ src/org/usfirst/frc/team4169/robot/commands/AutoCommand.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4169.robot.commands;
 
+import org.usfirst.frc.team4169.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
@@ -8,7 +10,7 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
  */
 public class AutoCommand extends CommandGroup {
 
-    public AutoCommand(int slot, int orient, int switchOrScale, double delay) {
+    public AutoCommand(int slot, int orient, int switchOrScale, double delay, int dirSOS) {
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
@@ -25,22 +27,37 @@ public class AutoCommand extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-    	
-    	int dist[] = {29, 149, 221, 101, 29, 90, -90, 26, 84, 108, 268, 28, 41};
-    	int a = dist[slot + 2 * orient - 1];
-    	int b = dist[orient + 5];
-    	//int c = dist[switchOrScale + 7];
-    	int d = dist[switchOrScale + 9];
-    	int e = dist[switchOrScale + 11];
+    	int dirMatch = Math.abs(switchOrScale - dirSOS);
+    	//			  0  1   2    3   4   5   6  7  8   9  10  11  12  13   14 15  16 17
+    	int dist[] = {29,149,221,101,29, 90,-90, 26,84, 17,30, 108,268,188, 70,80};
+    	int a = dist[slot + 2 * orient - 1]; //Distance to a side
+    	int b = dist[orient + 5]; //First turn
+    	int c = dist[switchOrScale + 7]; //Height of lift
+    	int d = dist[switchOrScale + 9]; //Distance from side to target (Switch or Scale)
+    	int e = dist[switchOrScale * (1 - dirMatch) + 2 * dirMatch + 11]; //Second drive distance
+    	int f = dist[switchOrScale + 14]; //Distance from crossing point to target
     	
     	addSequential(new WaitCommand(delay));
     	addSequential(new DriveToDistance(32));
     	addSequential(new TurnForDegrees(b));
     	addSequential(new DriveToDistance(a));
     	addSequential(new TurnForDegrees(-b));
-    	addSequential(new DriveToDistance(d));
-    	addSequential(new TurnForDegrees(-b));
-    	//addSequential(new liftToPosition(c);
     	addSequential(new DriveToDistance(e));
+    	
+    	addSequential(new TurnForDegrees(-b * dirMatch));
+    	addSequential(new DriveToDistance(254 * dirMatch));
+    	addSequential(new TurnForDegrees((2 * switchOrScale - 1) * b * dirMatch));
+    	addSequential(new DriveToDistance(f * dirMatch));
+    	
+    	addSequential(new TurnForDegrees(-b));
+    	addSequential(new DriveToDistance(d));
+    	addSequential(new MoveLiftToPosition(c));
+    	Robot.kGrabber.moveGrabber(-1);
+    	addSequential(new MoveLiftToPosition(-c));
+    	addSequential(new DriveToDistance(-(d + 16)));
+    	
+    	
+    	
+    	//addSequential(new liftToPosition(c);
     }
 }
